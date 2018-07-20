e5997c2c8a8296999444917be3a196aca1888005
Ryan Hall <ryanhall@Ryans-MacBook-Pro.local>
2018-07-20 13:03:46 -0600

    removed sass installs, using less instead

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
 import Navbar from './components/navbar.js';
 import MyInfo from './components/my-info.js';
-import woodenSign from './assets/woodenSign.svg';
-import theaterRoom from './assets/theaterRoom.jpg';
 import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container-fluid">
         <Navbar name="Ryan Hall" linkOne="Movies" linkTwo="Soccer" linkThree="Gaming"></Navbar>
 
-        <section className="hero-container">
-          <img src={theaterRoom} className="App-hero" alt="hero" />
+        <section className="hero-section">
+          <div className="hero-container"></div>
         </section>
 
         <MyInfo></MyInfo>
 
-        
+
       </div>
     );
   }
